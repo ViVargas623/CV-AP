654c4ab501bf44636663492f82fee3b57b34b0f1
victoria <vargasvictoria623@gmail.com>
2023-02-28 14:50:10 -0300

    cv

---
+++ javascript/eventos.js
@@ -0,0 +1,41 @@
+function enNegro1() {
+Element =document.getElementById("pers");
+Element.src="img/personal.icono2.png";
+}
+function enBlanco1() {
+    Element =document.getElementById("pers");
+    Element.src="img/personal.icono.png";
+}
+function enNegro2() {
+    Element =document.getElementById("labo");
+    Element.src="img/laboral.icono2.png";
+}
+ function enBlanco2() {
+    Element =document.getElementById("labo");
+    Element.src="img/laboral.icono.png";
+}
+function enNegro3() {
+    Element =document.getElementById("habi");
+    Element.src="img/habilidades.icono2.png";
+}
+function enBlanco3() {
+    Element =document.getElementById("habi");
+    Element.src="img/habilidades.icono.png";
+}
+function enNegro4() {
+    Element =document.getElementById("estu");
+    Element.src="img/estudios.icono2.png";
+}
+function enBlanco4() {
+    Element =document.getElementById("estu");
+    Element.src="img/estudios.icono.png";
+}
+function bigImg(x) {
+    x.style.height = "45px";
+    x.style.width = "45px";
+}
+  function normalImg(x) 
+  {
+    x.style.height = "32px";
+    x.style.width = "32px";
+ }
